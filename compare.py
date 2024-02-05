9b46448f1f986d5926bcba7f27d5e7c1230ad2bd
LacoutierePaul <paul.lacoutiere@gmail.com>
2024-02-05 16:35:41 +0100

    make a compare.py file to compare with the other of the group with ngrok

---
+++ compare.py
@@ -0,0 +1,31 @@
+import requests
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+
+paul_url ="https://9ee4-89-30-29-68.ngrok-free.app/predict"
+lazare_url="https://8aba-89-30-29-68.ngrok-free.app/predict"
+hubert_url="https://04d3-89-30-29-68.ngrok-free.app/predict"
+
+
+url=[paul_url,lazare_url,hubert_url]
+sepal_length = 0.1
+sepal_width = 1.5
+petal_length = 1.4
+petal_width = 0.2
+value=[]
+
+for i in url:
+    response = requests.get(f'{i}/{sepal_length}/{sepal_width}/{petal_length}/{petal_width}')
+    if response.status_code == 200:
+        print(f"URL: {i}")
+        print(response.json())
+        value.append(response.json())
+    else:
+        print(f'Request failed with status code: {response.status_code}')
+        print(response.text)
+
+
