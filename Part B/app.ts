5d4a1d2dc5f0cc775142e1389d255f3a4b008b7d
LacoutierePaul <paul.lacoutiere@gmail.com>
2024-02-05 18:30:15 +0100

    Create DNS, and connect to DB

--- Part B/app.ts
+++ Part B/app.ts
@@ -1,15 +1,30 @@
 // server.ts
 import * as express from 'express';
 import {Request, Response} from 'express';
-
+import {connectDb, createTables} from './DBManager';
 const app = express();
 app.use(express.json());
+
 const port = 3001;
 
+connectDb();
+createTables().then(r => {
+});
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
+});
+
+app.get('/getServer', (req, res) => {
+    const serverUrl = `localhost:${port}`;
+    res.json({ code: 200, server: serverUrl });
 });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+
+
+
