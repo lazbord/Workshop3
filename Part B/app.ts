6efcb686ab07e2c0f189baca8fe8ca5b7b7a1776
LacoutierePaul <paul.lacoutiere@gmail.com>
2024-02-05 17:02:20 +0100

    Create simple hello word server

---
+++ Part B/app.ts
@@ -0,0 +1,15 @@
+// server.ts
+import * as express from 'express';
+import {Request, Response} from 'express';
+
+const app = express();
+app.use(express.json());
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
